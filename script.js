0a8388ad102465c2b46e626d3dcde65a5303228f
Ilya Davydenko <57727023+SometimesUseful@users.noreply.github.com>
2021-06-24 12:49:33 +0300

    FInale

--- script.js
+++ script.js
@@ -11,18 +11,25 @@ function showWeather(city){
         alert('Enter a city');
         return;
     }
-    document.querySelector('.w-placeholder').style.visibility = 'visible';
     let date = new Date();
-    let cityData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f89e161a878685dcfa708dfbc08a670e`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f89e161a878685dcfa708dfbc08a670e`)
         .then(response => response.json())
         .then(data => {
+            if (data.name){
+                document.querySelector('.w-placeholder').style.visibility = 'visible';
+                document.querySelector('.w-placeholder').style.display = 'flex';
+                document.querySelector('.error-placeholder').style.visibility = 'hidden';
             document.querySelector('.city').innerText = data.name;
             document.querySelector('.weather-img').style.backgroundImage = `url(http://openweathermap.org/img/w/${data.weather[0].icon}.png)`;
             document.querySelector('.weather-desk').innerText = `Weather: ${data.weather[0].description}`;
             document.querySelector('.temp').innerText = `Temperature: ${Math.round(data.main.temp - 273.15)}°C`;
             document.querySelector('.wind').innerText = `Wind speed: ${data.wind.speed} m/s`;
             document.querySelector('.humidity').innerText = `Humidity: ${data.main.humidity}%`;
-
+            }else {
+                document.querySelector('.error-placeholder').style.visibility = 'visible';
+                document.querySelector('.w-placeholder').style.display = 'none';
+                document.querySelector('.err').innerText = `${data.message}`;
+            }
         })
         .catch(err => alert(err.message));
         let month;
@@ -32,8 +39,6 @@ function showWeather(city){
             month = `${date.getMonth()+1}`;
         }
         document.querySelector('.time').innerText = `${date.getDate()}.${month} ${date.getHours()}:${date.getMinutes()}`;
-
-
 }
 
 showBtn.addEventListener('click', () => showWeather(input.value));
